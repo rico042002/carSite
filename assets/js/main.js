67947c4eed06a8322ce70a19aad5fc9d575f9a80
Rico042002 <rico042002@gmail.com>
2023-10-10 04:02:34 -0500

    javascript added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,36 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
 
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
-
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
 
 /*=============== POPULAR SWIPER ===============*/
 
-
 /*=============== MIXITUP FILTER FEATURED ===============*/
 
+/* Link active featured */
 
-/* Link active featured */ 
-
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
